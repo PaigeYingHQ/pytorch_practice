32dc0069b787215f1d795983c13506baf0f03bd3
PaigeYingHQ <yinghq1006@foxmail.com>
2019-08-02 09:59:34 +0800

    add load_data_practice

--- src/load_data_practice.py
+++ src/load_data_practice.py
@@ -1,4 +1,6 @@
+import numpy
 import pandas as pd
+import torch
 from torch.utils import data
 
 
@@ -6,16 +8,25 @@ class CSVDataset(data.Dataset):
 
     def __init__(self, filename):
         super(CSVDataset, self).__init__()
-        self.csv = pd.read_csv(filename, iterator=True)
+        self.csv = pd.read_csv(filename)
+        self.x = self.csv.drop(columns='y')
+        self.y = self.csv['y']
 
     def __getitem__(self, index: int):
-        pass
+        x = torch.tensor(numpy.asarray(self.x.loc[index]))
+        y = torch.tensor(numpy.asarray(self.y.loc[index]))
+        return {'x':x,'y':y}
 
     def __len__(self):
-        pass
+        return self.csv.shape[0]
 
 
 if __name__ == '__main__':
 
-    csv_filepath = '../data/csv/'
+    csv_filepath = '../data/csv/linear.csv'
+    csv = CSVDataset(csv_filepath)
+    print('number of samples:',len(csv))
+    print(csv[1])
+
+
 
